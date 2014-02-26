2e6a473c18e81727823792b09bd2992b74e4b18b
Rowan Manning <rowan.manning@nature.com>
2014-02-26 13:43:56 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+// This file is part of bandiera-client-node.
+// 
+// bandiera-client-node is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// bandiera-client-node is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with bandiera-client-node.  If not, see <http://www.gnu.org/licenses/>.
+
+/* jshint maxlen: false, maxstatements: false */
+'use strict';
+
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+
+		jshint: {
+			all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+			options: {
+				es3: false,
+				indent: 4,
+				latedef: false,
+				maxcomplexity: 4,
+				maxdepth: 2,
+				maxlen: 100,
+				maxparams: 3,
+				maxstatements: 6,
+				node: true,
+				quotmark: 'single'
+			}
+		},
+
+		mochaTest: {
+			unit: {
+				src: ['test/unit/**/*.js'],
+				options: {
+					reporter: 'spec'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('test', ['mochaTest']);
+	grunt.registerTask('default', ['lint', 'test']);
+	grunt.registerTask('ci', ['lint', 'test']);
+
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+
+Bandiera Client (Node)
+======================
+
+This is a client for talking to the [Bandiera][bandiera] feature flagging service from a Node.js application.
+
+
+License
+-------
+
+[Copyright 2014 Nature Publishing Group](LICENSE.txt).  
+Bandiera is licensed under the [GNU General Public License 3.0][gpl].
+
+[gpl]: http://www.gnu.org/licenses/gpl-3.0.html
+[bandiera]: https://github.com/nature/bandiera
